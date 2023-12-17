973f02264739c7d76f51e503f0343bcb47a69b56
semen.yakushev@resheniya.org <semen.yakushev@resheniya.org>
2023-12-17 23:01:02 +0300

    Добавьте файлы проекта.

---
+++ Express API/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Пример базы данных
+let users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+    { id: 3, name: 'Bob' }
+];
+
+// Получить список всех пользователей
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+// Получить пользователя по id
+app.get('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+// Добавить нового пользователя
+app.post('/api/users', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.status(201).json(user);
+});
+
+// Обновить пользователя по id
+app.put('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (user) {
+        user.name = req.body.name;
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+// Удалить пользователя по id
+app.delete('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        const deletedUsers = users.splice(index, 1);
+        res.json(deletedUsers[0]);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
